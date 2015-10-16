f9d4f3b4df3157e05dbaf698ab62feb0bb1cd0f4
niku <niku@niku.name>
2015-10-16 10:09:49 +0900

    rebar3 3.0.0-beta.3

--- Formula/rebar3.rb
+++ Formula/rebar3.rb
@@ -3,9 +3,9 @@ class Rebar3 < Formula
   homepage "http://www.rebar3.org"
 
   devel do
-    url "https://github.com/rebar/rebar3/archive/beta-2.tar.gz"
-    version "3.0.0-beta-2"
-    sha256 "0aa014290f5d971206fc79f48d0e3047266e09a6124f044b7e6f7a396b10d831"
+    url "https://github.com/rebar/rebar3/archive/3.0.0-beta.3.tar.gz"
+    version "3.0.0-beta.3"
+    sha256 "8acfb89ed638e6ee0d364509ddacf2fbf1609f0c4de4eb1a775da4cfebe2053d"
 
     depends_on "erlang"
   end
